865296108724ec4970a9023fa2546e7c26163388
er-priyanka <shahanepriyankas01@gmail.com>
2023-01-21 17:03:23 +0530

    added css to trains components

--- frontend/shubhyatra/src/Components/Trains/TrainsTabs.jsx
+++ frontend/shubhyatra/src/Components/Trains/TrainsTabs.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Input, 
+import { Box, Button, Center, Flex, Input, 
     InputGroup, Radio, RadioGroup, 
     Select, Stack, Text,
  } from "@chakra-ui/react"
@@ -8,49 +8,79 @@ export const TrainsTabs = ()=>{
    
     return (
         <Box 
-        w="6xl"
-        m="auto"
-        p="40px 20px"
-        borderRadius="10px"
-        border="1px solid black">
-            <Flex 
-            justifyContent="space-between" 
+        bgColor="#0E25A8"
+        pb="20px"
+        >
+            <Box 
+            w="6xl"
+            m="auto"
+            p="40px 20px"
+            borderRadius="10px"
+            
+            bgColor= "white"
             >
-                    
-                <RadioGroup defaultValue="1">
-                    <Stack direction="row">
-                        <Radio value = "1">BOOK TRAIN TICKETS</Radio>
+                <Flex 
+                justifyContent="space-between" 
+                >
+                        
+                    <RadioGroup defaultValue="1">
+                        <Stack 
+                        color="blackAlpha.600"
+                        fontWeight="bold" 
+                        direction={{md:"row"}}
+                        gap="2"
+                        >
+                            <Radio value = "1">BOOK TRAIN TICKETS</Radio>
                             <Radio value = "2">CHECK PNR STATUS</Radio>
                             <Radio value = "3">LIVE TRAINS STATUS</Radio>
-                    </Stack>
-            
-                </RadioGroup>  
-                <Stack>
-                    <Text>IRCTC Train Ticket Booking</Text>
-                    <Text>IRCTC Authorized e-ticketing</Text>
-                </Stack>    
-            </Flex>
+                        </Stack>
+                
+                    </RadioGroup>  
+                    <Box>
+                        <Text
+                        fontSize="0.9rem"
+                        fontWeight="bold" 
+                        >IRCTC Train Ticket Booking</Text>
+                        <Text color="gray.500">IRCTC Authorized e-ticketing</Text>
+                    </Box>    
+                </Flex>
 
-            <Flex>
-                <Input type="text" value = "Delhi" placeholder="From" />
-                <Input type="text" value = "Mumbai" placeholder="To" />
-                <Input type = "date" />
+                <Flex
+                mt="20px"
+                gap="4"
+                >
+                    <Input type="text" value = "Delhi" placeholder="From" />
+                    <Input type="text" value = "Mumbai" placeholder="To" />
+                    <Input type = "date" />
 
-                <Select placeholder="Select class">
-                    <option value="All Class">All Class</option>
-                    <option value="Sleeper Class">Sleeper Class</option>
-                    <option value="Third AC">Third AC</option>
-                    <option value="Second AC">Second AC</option>
-                    <option value="First AC">First AC</option>
-                    <option value="Second Seating">Second Seating</option>
-                    <option value="AC Chair Car">AC Chair Car</option>
-                    <option value="First Class">First Class</option>
-                    <option value="Third AC Economy">Third AC Economy</option>
-                </Select>
-            </Flex>
+                    <Select placeholder="Select class">
+                        <option value="All Class">All Class</option>
+                        <option value="Sleeper Class">Sleeper Class</option>
+                        <option value="Third AC">Third AC</option>
+                        <option value="Second AC">Second AC</option>
+                        <option value="First AC">First AC</option>
+                        <option value="Second Seating">Second Seating</option>
+                        <option value="AC Chair Car">AC Chair Car</option>
+                        <option value="First Class">First Class</option>
+                        <option value="Third AC Economy">Third AC Economy</option>
+                    </Select>
+                </Flex>
 
-            <Button>SEARCH</Button>
+                <Center>
+                    <Button
+                    
+                    mt="20px"
+                    colorScheme="blue"
+                    px="50px"
+                    borderRadius="30px"
+                    >
+                    SEARCH
+                    </Button>
+                </Center>
+                
+            </Box>
         </Box>
+       
 
        
         
