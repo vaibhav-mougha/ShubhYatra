49dae87b444f0ee5287c6f498e409f9df40f15ab
sumit6675 <sumitchimkar0@gmail.com>
2023-01-21 22:05:12 +0530

    adding order model

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,8 @@ const { connection } = require("./configs/db");
 const {hotelsRoute}=require("./Routes/Hotels.Route")
 const { authenticate,AdminAuthenticate} =require("./Middlewares/authenticate");
 const { usersRoute } =require("./Routes/User.Route")
-const { flightRouter } = require("./Routes/Flight.route")
+const { flightRouter } = require("./Routes/Flight.route");
+const OrderRouter = require("./Routes/order.route");
 
 //Middlewares
 app.use(express.json());
@@ -19,6 +20,8 @@ app.use(
 );
 //user Route
 app.use("/users",usersRoute)
+//Order Route
+app.use("/order", OrderRouter)
 //Authenticate
 app.use(authenticate)
 //Admin Authenticate
@@ -29,6 +32,7 @@ app.use("/hotels",hotelsRoute)
 app.use("/bus",busRouter)
 // Booked Flight Route
 app.use("/bookedflight", flightRouter);
+//Order Route
 
 app.get("/", (req, res) => {
   res.send("Welcome Home Page");
