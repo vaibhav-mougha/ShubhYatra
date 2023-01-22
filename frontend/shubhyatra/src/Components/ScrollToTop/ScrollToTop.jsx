c2b6b1f906e7721a16f6fd206ef842e4a568ef69
Vaibhav Kumar Mougha <tunu1994@gmail.com>
2023-01-22 15:50:52 +0530

    Scroll To Top Functionality

--- frontend/shubhyatra/src/Components/ScrollToTop/ScrollToTop.jsx
+++ frontend/shubhyatra/src/Components/ScrollToTop/ScrollToTop.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useLocation } from "react-router";
 
 const ScrollToTop = (props) => {
+  // Helps in rendering the page from the TOP only
   const location = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
