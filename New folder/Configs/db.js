61d63e609e4ab528bcdda2538e63fcf0bfa2d27b
VAIBHAV MOUGHA <tunu1994@gmail.com>
2023-01-22 18:19:13 +0530

    Delete New folder directory

--- New folder/Configs/db.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const URL = process.env.MongoURl;
-const mongoose = require("mongoose");
-
-const connection = mongoose.connect(URL);
-
-module.exports = {
-  connection,
-};
